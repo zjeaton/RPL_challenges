ba27c848919b907711b93a47bf671dd535f6bacf
zjeaton <zjeaton@gmail.com>
2022-01-09 20:43:03 -0800

    add comments

--- ch_8/mean_median_mode/src/main.rs
+++ ch_8/mean_median_mode/src/main.rs
@@ -16,6 +16,7 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
+    // checks to ensure that every character in the string is a digit, comma, space, or dash.
     for c in input.trim().chars() {
         match c {
             '1'| '2'| '3' | '4' | '5' | '6' |'7' | '8' | '9' | '0' => (),
@@ -27,9 +28,15 @@ fn main() {
         }
 
     }
+    // replaces all commas with spaces in the input string
     let input = input.replace(",", " ");
 
     for num in input.split_whitespace() {
+        // since the - symbol can be placed anywhere within the string and it will pass
+        // the 'is it a number' test in the for loop above, the if let Ok(num) ensures that
+        // it actually is a valid number, with the - symbol at the beginning of the digits.
+        // num.parse::<isize>() will convert the split string to an isize, and it will push
+        // onto the vector if it is valid. 
         if let Ok(num) = num.parse::<isize>() {
             v.push(num);
         } else {
